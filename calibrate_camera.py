d4720d2d6252f35663b6a00f8e3678851cf9a767
begemotv2718 <begemotv2718@gmail.com>
2013-01-01 21:55:42 +0300

    Determine corner type for opencv features

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -30,6 +30,136 @@ def solve_camera(xyz, xy):
 xyz = n.array([[1,2,3],[3,4,5],[0,1,2],[4,5,6]])
 xy = n.array([[1,2],[1,3],[1,4],[2,7]])
 
+class UnionFind:
+    """ From http://code.activestate.com/recipes/215912/"""
+    def __init__(self):
+        '''\
+Create an empty union find data structure.'''
+        self.num_weights = {}
+        self.parent_pointers = {}
+        self.num_to_objects = {}
+        self.objects_to_num = {}
+        self.__repr__ = self.__str__
+    def insert_objects(self, objects):
+        '''\
+Insert a sequence of objects into the structure.  All must be Python hashable.'''
+        for object in objects:
+            self.find(object);
+    def find(self, object):
+        '''\
+Find the root of the set that an object is in.
+If the object was not known, will make it known, and it becomes its own set.
+Object must be Python hashable.'''
+        if not object in self.objects_to_num:
+            obj_num = len(self.objects_to_num)
+            self.num_weights[obj_num] = 1
+            self.objects_to_num[object] = obj_num
+            self.num_to_objects[obj_num] = object
+            self.parent_pointers[obj_num] = obj_num
+            return object
+        stk = [self.objects_to_num[object]]
+        par = self.parent_pointers[stk[-1]]
+        while par != stk[-1]:
+            stk.append(par)
+            par = self.parent_pointers[par]
+        for i in stk:
+            self.parent_pointers[i] = par
+        return self.num_to_objects[par]
+    def union(self, object1, object2):
+        '''\
+Combine the sets that contain the two objects given.
+Both objects must be Python hashable.
+If either or both objects are unknown, will make them known, and combine them.'''
+        o1p = self.find(object1)
+        o2p = self.find(object2)
+        if o1p != o2p:
+            on1 = self.objects_to_num[o1p]
+            on2 = self.objects_to_num[o2p]
+            w1 = self.num_weights[on1]
+            w2 = self.num_weights[on2]
+            if w1 < w2:
+                o1p, o2p, on1, on2, w1, w2 = o2p, o1p, on2, on1, w2, w1
+            self.num_weights[on1] = w1+w2
+            del self.num_weights[on2]
+            self.parent_pointers[on2] = on1
+    def length(self):
+        return len(self.num_weights)
+    def sets(self):
+        sets = {}
+        for x in self.objects_to_num.keys():
+          if self.find(x) in sets:
+            sets[self.find(x)].append(x)
+          else:
+            sets[self.find(x)]=[x]
+        return [sets[x] for x in sets.keys()]
+    def __str__(self):
+        '''\
+Included for testing purposes only.
+All information needed from the union find data structure can be attained
+using find.'''
+        sets = {}
+        for i in xrange(len(self.objects_to_num)):
+            sets[i] = []
+        for i in self.objects_to_num:
+            sets[self.objects_to_num[self.find(i)]].append(i)
+        out = []
+        for i in sets.itervalues():
+            if i:
+                out.append(repr(i))
+        return ', '.join(out)
+
+
+def cluster_points(values):
+  clusters = UnionFind()
+  l = range(len(values))
+  clusters.insert_objects(l)
+  pairs = [ (i,j) for i in range(len(values)) for j in range(i)]
+  spairs = sorted(pairs, reverse=True,key=lambda x: abs(values[x[0]]-values[x[1]]))
+  while(clusters.length()>2):
+    pair = spairs.pop()
+    clusters.union(pair[0],pair[1])
+  return clusters.sets()
+
+
+
+def GetCornerType(x,y,img):
+  """Get type of corner point: upper left, bottom left, upper right, bottom right, vertical half, horizontal half, 
+  also white on dark background or dark on white background. Greyscale image is presumed"""
+  x1=int(x)
+  y1=int(y)
+  height = 5
+  width = 5
+  print "x=", x1, "; y=", y1, "; width=", width, ";height=",height
+  print img
+  rect1 = cv.Avg(cv.GetSubRect(img,(x1,y1, width, height)))
+  rect2 = cv.Avg(cv.GetSubRect(img,(x1-width,y1,width, height)))
+  rect3 = cv.Avg(cv.GetSubRect(img,(x1-width,y1-height,width,height)))
+  rect4 = cv.Avg(cv.GetSubRect(img,(x1,y1-height,width,height)))
+  print "x=%d, y=%d :: 1: %f 2: %f 3: %f 4: %f" % (x,y,rect1[0],rect2[0],rect3[0],rect4[0])
+  averages = [rect1[0],rect2[0],rect3[0],rect4[0]]
+  clusters=cluster_points(averages)
+  if(len(clusters[0]) == 2):
+    return("bad","")
+  else:
+    fg,bg=None,None
+    corner=None
+    if(len(clusters[0])==1):
+      fg = averages[clusters[0][0]]
+      bg = averages[clusters[1][0]]
+      corner = clusters[0][0]
+    else:
+      fg = averages[clusters[1][0]]
+      bg = averages[clusters[0][0]]
+      corner = clusters[1][0]
+
+    if(fg>bg):
+      return (corner,"w")
+    else:
+      return (corner,"b")
+
+
+  
+
 
 
 
@@ -45,8 +175,13 @@ img32f = cv.CreateImage((img.width,img.height),cv.IPL_DEPTH_32F, 3)
 cv.Convert(img,img32f)
 img_gs=cv.CreateImage((img.width,img.height),cv.IPL_DEPTH_32F,1)
 cv.CvtColor(img32f,img_gs,cv.CV_RGB2GRAY)
-for (x,y) in cv.GoodFeaturesToTrack(img_gs, eig_image, temp_image, 10, 0.04, 3.0):
-  cv.Circle(img2,(int(x),int(y)),3,cv.RGB(155,0,2))
+font = cv.InitFont(cv.CV_FONT_HERSHEY_PLAIN,1.0,1.0)
+
+for (x,y) in cv.GoodFeaturesToTrack(img_gs, eig_image, temp_image, 50, 0.04, 3.0, blockSize=5):
+  ptype = GetCornerType(x,y,img_gs)
+  print ptype
+  cv.PutText(img2,"%s%s" % ptype,(int(x),int(y)),font,cv.RGB(100,100,0))
+  cv.Circle(img2,(int(x),int(y)),5,cv.RGB(155,0,2))
 
 cv.SaveImage("test.jpg",img2)
 
